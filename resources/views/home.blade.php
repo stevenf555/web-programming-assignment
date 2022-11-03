fcac1b34eb51ad883ade6aee71b8ae9a1865fbc0
Steven <stevenfz556@gmail.com>
2022-11-03 17:45:45 +0700

    New Assignment GSLC

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,42 +3,6 @@
 @section('Title', 'Home')
 
 @section('content')
-    @php
-    $images = [
-        'https://www.greenscene.co.id/wp-content/uploads/2022/08/luffy-bounty.jpg',
-        'https://i.pinimg.com/564x/9d/bd/d2/9dbdd20fbba3c2e587319623776d9f59.jpg',
-        'https://i.pinimg.com/736x/6a/fd/ad/6afdada20f304e49e38bc48bac92b9d9.jpg',
-        'https://wallpaperaccess.com/full/7518574.jpg',
-        'https://wallpaperaccess.com/full/6355116.png',
-        'https://i.pinimg.com/originals/ed/9d/09/ed9d09d3f2a116b592e4202c99dc922b.jpg',
-        'https://i.pinimg.com/originals/fa/44/46/fa4446287dc75342a8a4c215313db261.jpg',
-        'https://id-test-11.slatic.net/p/5a6ec69362e277437d3242208c23c38b.jpg',
-        'https://id-test-11.slatic.net/p/c13c1f7df4a64d05ec5b7cccdc51a5b7.jpg',
-        'https://id-test-11.slatic.net/p/d26a98b8af1a41bcebe202609294e6b3.jpg',
-        'https://i.pinimg.com/originals/ee/e2/cb/eee2cb2b72d0155730397c411f79f189.png',
-        'https://i.pinimg.com/originals/c4/34/40/c434407ea538caca74901c39c5c00a86.png',
-        'https://i.pinimg.com/originals/4e/f6/02/4ef602094d20c4320a08aa314a9e5fdb.jpg'
-        ]
-    @endphp
-
-    @php
-    $name = [
-        'Luffy',
-        'Zoro',
-        'Nami',
-        'Usopp',
-        'Sanji',
-        'Chopper',
-        'Robin',
-        'Marshall D Teach',
-        'Shanks',
-        'Big Mom',
-        'Kaido',
-        'Shirohige',
-        'Law'
-        ]
-    @endphp
-
     <div class="p-3">
     </div>
     <div class="ps-5 pe-5" style="height:1300px">
@@ -64,5 +28,4 @@
                 </div>
         </div>
     </div>
-
 @endsection
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,6 +1,9 @@
 <div>
         <div class="bg-dark d-flex">
-            <h5 class="text-light p-4">One Piece</h5>
+            <a href="/" style="text-decoration: none">
+              <h5 class="text-light p-4">One Piece</h5>
+            </a>
+            <a href="/" style="text-decoration: none">
               <p class="text-light mt-4">Home</p>
             </a>
         </div>
